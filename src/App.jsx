fd051e3405cc829a37e18a1e30cb7b9be1674d27
shashidhar-sqilco <shashidhar@sqilco.com>
2024-05-15 01:12:38 +0530

    improvements

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,20 @@ import { basicDark, basicDarkInit } from '@uiw/codemirror-theme-basic/dark';
 
 
 const state = EditorState.create({
-  doc: 'my source code',
+  doc: 'my sourcecode',
   extensions: [basicDark, javascript({ jsx: true })],
 });
 function App() {
-  const [code, setCode] = useState('');
-  const [output, setOutput] = useState('');
+  const [code, setCode] = useState('console.log("Hello World!")');
+  const [output, setOutput] = useState({output:"",textColor:""});
 
   const handleExecuteCode = async () => {
     try {
-      const response = await axios.post('http://localhost:3000/execute', { code });
-      setOutput(response.data.output);
+          setOutput({output:'Compiling your code...',textColor:"text-orange-500"});
+      const response = await axios.post('https://jscompiler.onrender.com/execute', { code });
+      setOutput({output:response.data.output,textColor:"text-green-500"});
     } catch (error) {
-      setOutput(`Error: ${error.response.data.error}`);
+      setOutput({output:`Error: ${error.response.data.error}`,textColor:"text-red-500"});
     }
   };
 
@@ -31,10 +32,18 @@ function App() {
     setCode(val);
   }, []);
   return (
-    <div className="flex flex-col w-screen h-screen bg-gray-100">
-      <div className="flex-1 w-full p-6 flex">
-        <div className="w-1/2 mr-4">
-          <div className="bg-white rounded-md shadow-md p-4">
+    <div className="flex flex-col w-screen min-h-screen bg-[#181818]">
+      <div className="flex justify-end p-4">
+        <button
+          onClick={handleExecuteCode}
+          className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        >
+          Run
+        </button>
+      </div>
+      <div className="flex-1 w-full flex flex-col md:flex-row">
+        <div className="md:w-1/2 md:mr-4">
+          <div className="bg-white rounded-md h-[50vh] shadow-md">
             <CodeMirror
               value={code}
     theme={basicDarkInit({
@@ -44,28 +53,22 @@ function App() {
     }
   })}
               onChange={onChange}
-              height="90vh" extensions={[javascript({ jsx: true })]}
+               extensions={[javascript({ jsx: true })]}
    
        
               showLineNumbers={true}
               className="rounded-md"
             />
+            
           </div>
         </div>
-        <div className="w-1/2">
-          <div className="bg-black text-green-500 rounded-md shadow-md p-4 h-full overflow-auto font-mono">
-            <pre>{output}</pre>
+        <div className="md:w-1/2 h-[50vh] md:h-[90vh] max-h-screen  ">
+          <div className={`bg-black ${output.textColor} rounded-md shadow-md p-4 h-full overflow-scroll font-mono no-scrollbar`}>
+            <pre>{output.output}</pre>
           </div>
         </div>
       </div>
-      <div className="flex justify-center p-4">
-        <button
-          onClick={handleExecuteCode}
-          className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
-        >
-          Execute Code
-        </button>
-      </div>
+      
     </div>
   );
 }
